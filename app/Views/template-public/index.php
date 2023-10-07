b6e8c653c04f785befc21955ba68464eb8600f82
MrcSatrio <marcsatrioff@gmail.com>
2023-10-07 22:23:59 +0700

    add topbar public

--- app/Views/template-public/index.php
+++ app/Views/template-public/index.php
@@ -2,151 +2,260 @@
 <html lang="id">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="Team">
+    <title>KNOWLEDGE MANAGEMENT SYSTEM</title>
 
-    <title>?</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    
+    
 
-    <!-- Custom fonts for this template-->
-    <link href="<?= base_url() ?>/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <!-- Font Awesome Icons -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
 
-    <!-- Custom styles for this template-->
+    <!-- DataTables CSS -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.dataTables.min.css">
+
+
+    <!-- Custom CSS -->
     <link href="<?= base_url() ?>/assets/css/sb-admin-2.min.css" rel="stylesheet">
 
-    <!-- CSS sweetalert2 -->
-    <link href="<?= base_url() ?>/assets/css/sweetalert2.min.css" rel="stylesheet">
+    <!-- Custom Font -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
-    <!-- Custom styles for this page -->
-    <link href="<?= base_url() ?>/assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <!-- SweetAlert2 JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <!-- TinyMce -->
+    <script src="https://cdn.tiny.cloud/1/hgys0rz09d359u4mdu9g544jc35j7ixlp04uv0k4a7pbsxor/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Clipboard.js -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.8/clipboard.min.js"></script>
+
+    <!-- DataTables JS -->
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
 
 </head>
 
 <body id="page-top">
+<body id="page-top">
 
-    <!-- Page Wrapper -->
-    <div id="wrapper">
+<!-- Page Wrapper -->
+<div id="wrapper">
 
-        <!-- Content Wrapper -->
-        <div id="content-wrapper" class="d-flex flex-column">
+    <!-- Sidebar -->
+    <!-- End of Sidebar -->
 
-            <!-- Main Content -->
-            <div id="content">
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
 
-                <!-- Topbar -->
-                <?= $this->include('template-public/topbar'); ?>
-                <!-- End of Topbar -->
+        <!-- Main Content -->
+        <div id="content">
 
-                <!-- Begin Page Content -->
-                <div class="container-fluid">
-                    <?= $this->renderSection('container'); ?>
-                </div>
-                <!-- /.container-fluid -->
+        
+        <?= $this->include('template-public/topbar'); ?>
 
+            <!-- Begin Page Content -->
+            <div class="container-fluid">
+                <?= $this->renderSection('container'); ?>
             </div>
-            <!-- End of Main Content -->
-
-            <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2021</span>
-                    </div>
-                </div>
-            </footer>
-            <!-- End of Footer -->
+            <!-- /.container-fluid -->
 
         </div>
-        <!-- End of Content Wrapper -->
+        <!-- End of Main Content -->
+
+        <!-- Footer -->
+        <footer class="sticky-footer bg-white">
+            <div class="container my-auto">
+                <div class="copyright text-center my-auto">
+                    <span>Copyright &copy; I-BINA KNOW 2023.</span>
+                </div>
+            </div>
+        </footer>
+        <!-- End of Footer -->
 
     </div>
-    <!-- End of Page Wrapper -->
+    <!-- End of Content Wrapper -->
 
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
+</div>
+<!-- End of Page Wrapper -->
 
-    <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="/logout">Logout</a>
-                </div>
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+<!-- Logout Modal-->
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+            <div class="modal-footer">
+                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                <a class="btn btn-primary" href="/logout">Logout</a>
             </div>
         </div>
     </div>
-
-    <!-- Bootstrap core JavaScript-->
-    <script src="<?= base_url() ?>/assets/vendor/jquery/jquery.min.js"></script>
-
-    <script src="<?= base_url() ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="<?= base_url() ?>/assets/vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="<?= base_url() ?>/assets/js/sb-admin-2.min.js"></script>
-
-    <!-- Page level plugins -->
-    <script src="<?= base_url() ?>/assets/vendor/chart.js/Chart.min.js"></script>
-    <script src="<?= base_url() ?>/assets/vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="<?= base_url() ?>/assets/vendor/datatables/dataTables.bootstrap4.min.js"></script>
-
-    <!-- Page level custom scripts -->
-    <script src="<?= base_url() ?>/assets/js/demo/chart-area-demo.js"></script>
-    <script src="<?= base_url() ?>/assets/js/demo/chart-pie-demo.js"></script>
-    <script src="<?= base_url() ?>/assets/js/demo/datatables-demo.js"></script>
-
-    <!-- sweetalert2 js -->
-    <script src="<?= base_url() ?>/assets/js/sweetalert2.min.js"></script>
-
+</div>
+</body>
 
 <style>
-    /* Style untuk container */
-    .filter-container {
-        display: flex;
-        align-items: center;
-        gap: 10px;
+    /* Custom styles */
+    body {
+        background-color: #f8f9fa;
+    }
+
+    .topbar {
+        background-color: #fff;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
+
+    .logo {
+        font-weight: bold;
+        color: #343a40;
+    }
+
+    .logo svg {
+        fill: #17a2b8;
+        width: 40px;
+        height: 32px;
+        margin-right: 8px;
+    }
+
+    .nav-link {
+        color: #343a40;
+    }
+
+    .nav-link:hover {
+        color: #17a2b8;
+    }
+
+    .card {
+        border: none;
+        transition: transform 0.2s;
+    }
+
+    .card:hover {
+        transform: translateY(-5px);
+    }
+
+    .smaller-text {
+        font-size: 12px;
+    }
+
+    .title {
+        margin-bottom: 5px;
+    }
+
+    .upload-form {
+        background-color: #fff;
+        padding: 20px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+    }
+
+    .upload-form label {
+        display: block;
+        margin-bottom: 10px;
+        font-weight: bold;
+    }
+
+    .upload-form input[type="file"] {
         margin-bottom: 20px;
     }
 
-    /* Style untuk input date */
-    #dateFrom,
-    #dateTo {
-        padding: 8px;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-    }
-
-    /* Style untuk tombol filter */
-    #filterButton {
-        padding: 8px 15px;
-        background-color: #007bff;
+    .upload-form input[type="submit"] {
+        padding: 10px 15px;
+        background-color: #333;
         color: #fff;
         border: none;
-        border-radius: 4px;
+        border-radius: 5px;
         cursor: pointer;
     }
 
-    #filterButton:hover {
-        background-color: #0056b3;
+    select {
+        padding: 10px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        width: 100%;
     }
+
+    option {
+        background-color: #f5f5f5;
+        color: #333;
+}
+  /* Mengatur lebar dan tinggi carousel */
+  #myCarousel {
+    width: auto; /* Ubah sesuai dengan lebar yang Anda inginkan */
+    height: 100%; /* Ubah sesuai dengan tinggi yang Anda inginkan */
+}
+
+/* Mengatur ukuran gambar dalam carousel */
+.carousel-inner img {
+    width: 100%; /* Gambar akan mengisi seluruh lebar carousel */
+    height: 400px; /* Gambar akan mengisi seluruh tinggi carousel */
+    object-fit: cover; /* Untuk memastikan gambar selalu mengisi carousel */
+}
+
 </style>
 
-</body>
+<script>
+      tinymce.init({
+        selector: '#mytextarea',
+        plugins: [
+  'advlist', 'autolink', 'link', 'image', 'lists', 'charmap', 'preview', 'anchor', 'pagebreak',
+  'searchreplace', 'wordcount', 'visualblocks', 'visualchars', 'code', 'fullscreen', 'insertdatetime',
+  'media', 'table', 'emoticons', 'template', 'help'
+],
+      });
+    </script>
 
+  </head>
+  <?php
+$flashsuccess = session()->getFlashdata('success');
+$flasherror = session()->getFlashdata('error');
+?>
+
+<?php if (!empty($flashsuccess) || !empty($flasherror)): ?>
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        <?php if (!empty($flashsuccess)): ?>
+        Swal.fire({
+            icon: 'success',
+            title: 'Success',
+            html: '<?= esc($flashsuccess) ?>',
+            confirmButtonText: 'OK'
+        });
+        <?php endif; ?>
+
+        <?php if (!empty($flasherror)): ?>
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            html: '<?php if (is_array($flasherror)): ?><ul><?php foreach ($flasherror as $error): ?><li><?= esc($error) ?></li><?php endforeach; ?></ul><?php else: ?><?= esc($flasherror) ?><?php endif; ?>',
+            confirmButtonText: 'OK'
+        });
+        <?php endif; ?>
+    });
+</script>
+<?php endif; ?>
 </html>
