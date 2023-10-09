1fe5f0026c73cbd577934ded8dee3e45878871c2
Arief Fadhillah R <arief.fadhillah.r@gmail.com>
2023-10-09 20:52:03 +0700

    fix model

--- app/Models/AkunModel.php
+++ app/Models/AkunModel.php
@@ -26,31 +26,4 @@ class AkunModel extends Model
         'updated_at',
     ];
 
-    protected $validationRules = [
-        'email'     =>'required',
-        'password'     =>'required|alpha|max_length[32]|min_length[8]',
-        'nama'     =>'required|alpha_space|max_length[64]',
-        'telp'     =>'max_length[13]|numeric',
-    ]
-
-    protected $validationMessages = [
-        'email' => [
-            'required'      => 'Mohon mengisi kolom Email',
-        ],
-        'password' => [
-            'required'      => 'Password tidak boleh kosong',
-            'min_length'    => 'Karakter tidak boleh kurang dari 8 karakter',
-            'max_length'    => 'Karakter tidak boleh lebih dari 25 karakter'
-        ],
-        'nama' => [
-            'required'      => 'Mohon mengisi kolom Nama',
-            'alpha_space'   => 'Kolom harus berupa huruf',
-            'max_length'    => 'Karakter tidak boleh lebih dari 64 huruf',
-        ],
-        'telp' => [
-            'numeric'       => 'Kolom harus berupa angka',
-            'max_length'    => 'Karakter tidak boleh lebih dari 13 angka',
-        ],
-    ];
-
 }
--- app/Models/ProdukModel.php
+++ app/Models/ProdukModel.php
@@ -26,36 +26,4 @@ class ProdukModel extends Model
         'updated_at',
     ];
 
-    protected $validationRules = [
-        'sku'     =>'required|alpha_numeric',
-        'nama_produk'     =>'required|alpha|max_length[25]|is_unique[produk.nama_produk]',
-        'deskripsi'     =>'required|alpha_numeric_punct|max_length[255]',
-        'stok'     =>'required',
-        'berat'     =>'required|max_length[15]',
-    ]
-
-    protected $validationMessages = [
-        'sku' => [
-            'required'      => 'Mohon mengisi kolom Sku',
-            'alpha_numeric' => 'Kolom harus berupa huruf dan angka',
-        ],
-        'nama_produk' => [
-            'required' => 'Mohon mengisi kolom Nama Produk'
-            'alpha' => 'Kolom harus berupa Huruf A-Z',
-            'max_length' => 'Karakter tidak boleh lebih dari 25 huruf',
-            'is_unique' => 'Maaf, Produk sudah terdaftar',
-        ],
-        'deskripsi' => [
-            'required' => 'Mohon mengisi kolom Deskripsi'
-            'alpha_numeric_punct' => 'Kolom harus berupa Huruf dan Angka',
-            'max_length' => 'Karakter tidak boleh lebih dari 255 huruf',
-        ],
-        'stok' => [
-            'required' => 'Mohon mengisi kolom Stok'
-        ],
-        'berat' => [
-            'required' => 'Mohon mengisi kolom Berat Produk'
-        ],
-    ];
-
 }
