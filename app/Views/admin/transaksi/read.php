69176c8cba828d119d0a84af9fa92fb87dfe3028
Arief Fadhillah R <arief.fadhillah.r@gmail.com>
2023-10-17 20:23:07 +0700

    merge

--- app/Views/admin/transaksi/read.php
+++ app/Views/admin/transaksi/read.php
@@ -2,23 +2,21 @@
 
 <?= $this->section('container'); ?>
 
-
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
     <h1 class="h3 mb-0 text-gray-800">Riwayat Transaksi</h1>
-
 </div>
 
 <div class="card shadow mb-4">
     <div class="card-body">
         <div class="table-responsive">
             <table class="table align-middle mb-0 bg-white" id="dataTable">
-            <thead class="bg-light">
+                <thead class="bg-light">
                     <tr>
                         <th style="width: 5%;">No</th>
                         <th>Id Transaksi</th>
                         <th>Resi</th>
                         <th>Alamat</th>
-                        <th>Produk</th>
+                        <th style="width: 50%;">Produk</th>
                         <th>Catatan</th>
                         <th>Total Pembayaran</th>
                         <th style="width: 15%;">Action</th>
@@ -36,11 +34,17 @@
                             <td><?= $addr['resi'] ?></td>
                             <td><?= $addr['alamat'] ?><br><?= $addr['kota'] ?><br><?= $addr['kode_pos'] ?><br></td>
                             <?php $addressDisplayed = true; ?>
-                                <?php endif; ?>
+                            <?php endif; ?>
                             <?php endforeach; ?>
-                            <?php foreach ($tr['items'] as $item): ?>
-                                <td><?= $item['nama_produk'] ?></td>
-                                <td><?= $item['catatan'] ?></td>
+                            <?php $rowspan = count($tr['items']); ?>
+                            <?php foreach ($tr['items'] as $key => $item): ?>
+                            <?php if ($key === 0): ?>
+                            <td rowspan="<?= $rowspan ?>"><?= $item['nama_produk'] ?></td>
+                            <td rowspan="<?= $rowspan ?>"><?= $item['catatan'] ?></td>
+                            <?php else: ?>
+                            <td><?= $item['nama_produk'] ?></td>
+                            <td><?= $item['catatan'] ?></td>
+                            <?php endif; ?>
                             <?php endforeach; ?>
                             <td><?= number_format($tr['items'][0]['total_harga'], 0, ',', '.') ?></td>
                             <td>
@@ -49,13 +53,11 @@
                                 </a>
                             </td>
                         </tr>
-                        
                     <?php endforeach; ?>
                 </tbody>
             </table>
         </div>
     </div>
 </div>
-
 
 <?= $this->endSection(); ?>
